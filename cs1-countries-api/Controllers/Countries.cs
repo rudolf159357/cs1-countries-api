d3849fb1ea9d163d047192364d67cba1e94e4fe1
rudolf159357 <99330946+rudolf159357@users.noreply.github.com>
2023-02-19 12:49:43 +0100

    update controller code, using IActionResult

--- cs1-countries-api/Controllers/Countries.cs
+++ cs1-countries-api/Controllers/Countries.cs
@@ -6,52 +6,64 @@ using Microsoft.EntityFrameworkCore;
 namespace cs1_countries_api.Controllers
 {
     [ApiController]
-    public class Countries
+    public class Countries: ControllerBase
     {
         public AppDbContext _context { get; set; }
 
         public Countries(AppDbContext context) {
             _context = context;
         }  
-
+        
+        
+        //GET
         [HttpGet]
         [Route("api/[controller]/europe")]
-        public async Task<ActionResult<IEnumerable<EuropeModel>>> Europe() {
-            return await _context.EuropeCountries.ToListAsync();
+        public async Task<IActionResult> Europe() {
+            var list =  await _context.EuropeCountries.ToListAsync();
+            return Ok(list);
         }
 
+        //GET BY ID
         [HttpGet]
         [Route("api/[controller]/europe/{id}")]
-        public async Task<ActionResult<EuropeModel>> EuropeId(int? id) {
-            return await _context.EuropeCountries.FindAsync(id);
+        public async Task<IActionResult> EuropeId(int id) {
+            var data =  await _context.EuropeCountries.FindAsync(id);
+            return Ok(data);
         }
 
+        //GET
         [HttpGet]
         [Route("api/[controller]/america")]
-        public async Task<ActionResult<IEnumerable<AmericaModel>>> America() { 
-            return await _context.AmericaCountries.ToListAsync();
+        public async Task<IActionResult> America() { 
+            var list = await _context.AmericaCountries.ToListAsync();
+            return Ok(list);
         }
 
+        //GET BY ID
         [HttpGet]
         [Route("api/[controller]/america/{id}")]
-        public async Task<ActionResult<AmericaModel>> AmericaId(int? id)
+        public async Task<IActionResult> AmericaId(int id)
         {
-            return await _context.AmericaCountries.FindAsync(id);
+            var data =  await _context.AmericaCountries.FindAsync(id);
+            return Ok(data);
         }
 
+        //GET
         [HttpGet]
         [Route("api/[controller]/asia")]
-        public async Task<ActionResult<IEnumerable<AsiaModel>>> Asia()
+        public async Task<IActionResult> Asia()
         {
-            return await _context.AsiaCountries.ToListAsync();
+            var list = await _context.AsiaCountries.ToListAsync();
+            return Ok(list);
         }
 
-
+        //GET BY ID
         [HttpGet]
         [Route("api/[controller]/asia/{id}")]
-        public async Task<ActionResult<AsiaModel>> AsiaId(int? id)
+        public async Task<IActionResult> AsiaId(int id)
         {
-            return await _context.AsiaCountries.FindAsync(id);
+            var data =  await _context.AsiaCountries.FindAsync(id);
+            return Ok(data);
         }
     }
 }
